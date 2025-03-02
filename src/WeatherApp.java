21607e50e61e5e67fc6811d8a39d7f265cb5a15c
adintoholj <adin.toholj@gmail.com>
2025-03-02 20:40:38 +0100

    weatherapp fixes

--- src/WeatherApp.java
+++ src/WeatherApp.java
@@ -1,5 +1,6 @@
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -9,24 +10,40 @@ import java.util.Scanner;
 public class WeatherApp {
     public static JSONObject getWeatherData(String locationName){
         JSONArray locationData = getLocationData(locationName);
+
+        return null;
     }
 
-    private static JSONArray getLocationData(String locationName){
+    public static JSONArray getLocationData(String locationName){
         locationName = locationName.replaceAll(" ", "+");
         String urlString = "https://geocoding-api.open-meteo.com/v1/search?name=" + locationName + "&count=10&language=en&format=json";
-    }
+        try{
+            HttpURLConnection kon = fetchApiResponse(urlString);
+            if(kon.getResponseCode() != 200){
+                System.out.println("Error, can't connect to the API");
+                return null;
+            }else{
+                StringBuilder resultJson = new StringBuilder();
+                Scanner scanner = new Scanner(kon.getInputStream());
 
-    try{
-        HttpURLConnection kon = fetchApiResponse(urlString);
-        if(kon.getResponseCode() != 200){
-            System.out.println("Error, can't connect to the API");
-            return null;
-        }else{
-            StringBuilder resultJson = new StringBuilder();
-            Scanner scanner = new Scanner(kon.getInputStream());
+                while(scanner.hasNext()){
+                    resultJson.append(scanner.nextLine());
+                }
+
+                scanner.close();
+                kon.disconnect();
+
+                JSONParser parser = new JSONParser();
+                JSONObject resultJsonObject = (JSONObject) parser.parse(String.valueOf(resultJson));
+
+                JSONArray locationData = (JSONArray) resultJsonObject.get("results");
+                return locationData;
+            }
+        }catch(Exception e){
+            e.printStackTrace();
         }
-    }catch(Exception e){
-        e.printStackTrace();
+
+        return null;
     }
 
     private static HttpURLConnection fetchApiResponse(String urlString){
